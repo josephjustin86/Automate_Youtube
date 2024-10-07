76c45ca63e73649eb01434f042c1daa042d3c7de
Joseph Justin <josephjustin@Josephs-MacBook-Air.local>
2024-10-07 19:01:58 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -22,6 +23,7 @@ import java.util.logging.Level;
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import dev.failsafe.internal.util.Assert;
 
 public class TestCases extends ExcelDataProvider{ // Lets us read the data
         ChromeDriver driver;
@@ -31,6 +33,101 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
          * Follow `testCase01` `testCase02`... format or what is provided in
          * instructions
          */
+        
+
+        @Test
+        public void testCase01() throws InterruptedException {
+
+                System.out.println("Start Test case: testCase01");
+                Wrappers navigate = new Wrappers(driver);
+                navigate.navigateToSite("https://www.youtube.com/");
+                Thread.sleep(3000);
+                Boolean webLinkLoc = navigate.verifyWebLink("youtube");
+                Assert.isTrue(webLinkLoc, "Not able to load youtube");
+                navigate.navigateButton(By.xpath("//a[text()='About']"));
+                Boolean aboutPage = navigate.verifyWebLink("about");
+                Assert.isTrue(aboutPage, "Not able to load about page");
+                navigate.displayMessage();
+                System.out.println("End Test case: testCase01");
+        }
+
+        @Test
+        public void testCase02() throws InterruptedException {
+
+                System.out.println("Start Test case: testCase02");
+
+                Wrappers navigate = new Wrappers(driver);
+                navigate.navigateToSite("https://www.youtube.com/");
+                Thread.sleep(3000);
+                Boolean webLinkLoc = navigate.verifyWebLink("youtube");
+                Assert.isTrue(webLinkLoc, "Not able to load youtube");
+                navigate.navigateButton(By.xpath("//yt-formatted-string[contains(text(), 'Movies') or contains(text(), 'Films')]"));
+                Thread.sleep(3000);
+                navigate.clickTillDissapear(By.xpath("//button[@class='yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default yt-spec-button-shape-next--enable-backdrop-filter-experiment' and @aria-label='Next']"), 3);
+                navigate.checkMatureContent(By.xpath("//span[@id='title' and contains(text(), 'Top selling')]//..//..//..//..//..//..//..//ytd-grid-movie-renderer[last()]//p[last()]/../../div[2]/p"));
+                navigate.movieCategory(By.xpath("//span[@id='title' and contains(text(), 'Top selling')]//..//..//..//..//..//..//..//ytd-grid-movie-renderer[last()]//span[@class='grid-movie-renderer-metadata style-scope ytd-grid-movie-renderer']"));
+                System.out.println("End Test case: testCase02");
+        }
+
+        @Test
+        public void testCase03() throws InterruptedException {
+
+                System.out.println("Start Test case: testCase03");
+                Wrappers navigate = new Wrappers(driver);
+                navigate.navigateToSite("https://www.youtube.com/");
+                Thread.sleep(3000);
+                Boolean webLinkLoc = navigate.verifyWebLink("youtube");
+                Assert.isTrue(webLinkLoc, "Not able to load youtube");
+                navigate.navigateButton(By.xpath("//yt-formatted-string[contains(text(), 'Music')]"));
+                Thread.sleep(3000);
+                navigate.clickTillDissapear(By.xpath("//button[@class='yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default yt-spec-button-shape-next--enable-backdrop-filter-experiment' and @aria-label='Next']"), 2);
+                Thread.sleep(1000);
+                navigate.playlistName(By.xpath("(//span[@class='style-scope ytd-shelf-renderer' and contains(text(), \"India's Biggest Hits\")]//..//..//..//..//..//..//..)[1]//ytd-compact-station-renderer[last()]//h3"));
+                Thread.sleep(1000);
+                navigate.trackNumber(By.xpath("(//span[@class='style-scope ytd-shelf-renderer' and contains(text(), \"India's Biggest Hits\")]//..//..//..//..//..//..//..)[1]//ytd-compact-station-renderer[last()]//p[@id='video-count-text']"));
+                System.out.println("End Test case: testCase03");
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException {
+
+                System.out.println("Start Test case: testCase04");
+                Wrappers navigate = new Wrappers(driver);
+                navigate.navigateToSite("https://www.youtube.com/");
+                Thread.sleep(3000);
+                Boolean webLinkLoc = navigate.verifyWebLink("youtube");
+                Assert.isTrue(webLinkLoc, "Not able to load youtube");
+                navigate.navigateButton(By.xpath("//yt-formatted-string[contains(text(), 'News')]"));
+                Thread.sleep(6000);
+                navigate.navigateNews(By.xpath("//span[@id='title' and contains(text(), 'Latest news posts')]//..//..//..//..//..//..//..//..//.."));
+                System.out.println("End Test case: testCase04");
+        }
+
+        @Test(dataProvider="searchDataProvider")
+        public void testCase05(String searchElement) throws InterruptedException {
+
+                System.out.println("Start Test case: testCase05");
+                Wrappers navigate = new Wrappers(driver);
+                navigate.navigateToSite("https://www.youtube.com/");
+                Thread.sleep(3000);
+                Boolean webLinkLoc = navigate.verifyWebLink("youtube");
+                Assert.isTrue(webLinkLoc, "Not able to load youtube");
+                navigate.searchTopic(searchElement);
+                navigate.viewCount(By.xpath("//ytd-video-renderer[@class='style-scope ytd-item-section-renderer']//span[@class='inline-metadata-item style-scope ytd-video-meta-block'][1]"));
+                System.out.println("End Test case: testCase05");
+        }
+
+        @DataProvider(name = "searchDataProvider")
+        public Object[][] searchData(){
+        return new Object[][] 
+        {
+                { "Movies" },
+
+                { "Music" },
+
+                { "Games" }
+        };
+        }
 
         /*
          * Do not change the provided methods unless necessary, they will help in
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,17 +1,159 @@
 package demo.wrappers;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
 import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static Object Wrappers;
+    ChromeDriver driver;
+
+    public Wrappers(ChromeDriver driver) {
+        this.driver = driver;
+    }
+    
+    public void navigateToSite(String webLink) throws InterruptedException {
+        System.out.println("Navigating to " + webLink);
+        driver.get(webLink);
+    }
+
+    public Boolean verifyWebLink(String webName) throws InterruptedException {
+        Boolean status = driver.getCurrentUrl().contains(webName);
+        return status;
+    }
+
+    public void navigateButton(By locator) throws InterruptedException {
+        WebElement button = driver.findElement(locator);
+        button.click();
+    }
+
+    public void displayMessage() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[contains(text(), 'About YouTube')]")));
+        List<WebElement> messageElements = driver.findElements(By.xpath("//p[@class='lb-font-display-3 lb-font-color-text-primary']"));
+        for (WebElement messageElement : messageElements){
+            System.out.println(messageElement.getText());
+        }
+    }
+
+    public void clickTillDissapear(By locator, Integer maxIteration) throws InterruptedException {
+        Integer numIter = 0;
+        while(numIter < maxIteration){
+            try{
+                WebElement button = driver.findElement(locator);
+                button.click();
+                Thread.sleep(2000);
+            }
+            catch (Exception e){
+                System.out.println("Stopping - " + e.getMessage());
+                break;
+            }
+        }
+    }
+
+    public void checkMatureContent(By locator) throws InterruptedException {
+        WebElement matureString = driver.findElement(locator);
+        String matureContent = matureString.getText();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertNotEquals(matureContent, "A", "Movie is not for Mature content");
+        softAssert.assertAll();
+    }   
+
+    public void movieCategory(By locator) throws InterruptedException {
+        WebElement movieString = driver.findElement(locator);
+        String movieContent = movieString.getText();
+        SoftAssert softAssert = new SoftAssert();
+        Boolean status = movieContent.contains("Comedy") || movieContent.contains("Animation") | movieContent.contains("Drama");
+        softAssert.assertTrue(status, "Movie catogory exist");
+        softAssert.assertAll();
+    }  
+
+    public void playlistName(By locator) throws InterruptedException {
+        WebElement playlistName = driver.findElement(locator);
+        System.out.println("Playlist: "+playlistName.getText());
+    }  
+    
+    public void trackNumber(By locator) throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        WebElement trackElement = driver.findElement(locator);
+        String trackString = trackElement.getText();
+        String updatedTrackString = trackString.replaceAll("[^0-9]", "");
+        Integer trackInteger = Integer.parseInt(updatedTrackString);
+        System.out.println("Track count: " + trackInteger);
+        softAssert.assertTrue(trackInteger <= 50, "Number of tracks listed is more than 50");
+        softAssert.assertAll();
+    }  
+
+    public void navigateNews(By locator) throws InterruptedException {
+        WebElement newsElement = driver.findElement(locator);
+        Integer count = 0;
+        List<WebElement> newsElements = newsElement.findElements(By.xpath(".//ytd-rich-item-renderer"));
+        for (WebElement element : newsElements){
+            String titleElement = element.findElement(By.xpath(".//span[@class='style-scope ytd-post-renderer']")).getText();
+            String bodyElement = element.findElement(By.xpath(".//div[@id='post-text']//yt-formatted-string[@id='home-content-text']//span[@class='style-scope yt-formatted-string'][1]")).getText();
+            String likesElement = element.findElement(By.xpath(".//span[@id='vote-count-middle']")).getText();
+            String updatedLikesElement = likesElement.replaceAll("[^0-9]", "");
+            Integer likesNumber = Integer.parseInt(updatedLikesElement);
+            count += likesNumber;
+            System.out.println("Title: "+ titleElement);
+            System.out.println("Body: "+ bodyElement);
+            System.out.println("Likes: "+ likesElement);
+            System.out.println("\n");
+        }
+        System.out.println("Total likes: " + count);
+    }
+
+    public void searchTopic(String searchElement) throws InterruptedException {
+        WebElement searchText = driver.findElement(By.xpath("//input[@id='search']"));
+        searchText.sendKeys(searchElement);
+        Thread.sleep(3000);
+        WebElement searchButton = driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
+        searchButton.click();
+        Thread.sleep(5000);
+    } 
+
+    public void viewCount(By locator) throws InterruptedException {
+        Integer totalViewCount = 0;
+        List<WebElement> viewCountElements = driver.findElements(locator);
+        for (WebElement viewCountElement : viewCountElements){
+            String viewCountNumber = viewCountElement.getText();
+            if (viewCountNumber.contains("M")){
+                String preViewCountNumber = viewCountNumber.replaceAll("[^0-9]", "");
+                Integer newPreViewCountNumber = Integer.parseInt(preViewCountNumber);
+                newPreViewCountNumber = newPreViewCountNumber * 1000000;
+                totalViewCount += newPreViewCountNumber;
+            }
+            else if (viewCountNumber.contains("K")){
+                String preViewCountNumber = viewCountNumber.replaceAll("[^0-9]", "");
+                Integer newPreViewCountNumber = Integer.parseInt(preViewCountNumber);
+                newPreViewCountNumber = newPreViewCountNumber * 1000;
+                totalViewCount += newPreViewCountNumber;
+            }
+            else {
+                String preViewCountNumber = viewCountNumber.replaceAll("[^0-9]", "");
+                Integer newPreViewCountNumber = Integer.parseInt(preViewCountNumber);
+                totalViewCount += newPreViewCountNumber;
+            }
+
+            if (totalViewCount > 100000000)
+                break;
+        }
+
+        System.out.println("View count has reached above 1Cr");
+    } 
 }
